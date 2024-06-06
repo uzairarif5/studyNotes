35bb3dd9ef86731535c84b7fed0da6d7e5eeaf5d
Uzair <uzair.99@hotmail.com>
2024-06-05 22:18:27 -0400

    update 17.12

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export default function App(){
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/*" element={<Article />} />
+				<Route path="/worksheet" element={<Worksheet />} />
 			</Routes>
 		</BrowserRouter>
 		<Outlet/>
